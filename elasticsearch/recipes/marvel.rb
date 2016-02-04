983a5a0747718729a028d25b96eb48160d15aee7
polloelastico <merv@bigdumbtruck.com>
2016-02-04 11:27:08 -0800

    remove version from marvel-agent

--- elasticsearch/recipes/marvel.rb
+++ elasticsearch/recipes/marvel.rb
@@ -1,3 +1,3 @@
 node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
 
-install_plugin "marvel-agent/#{node.elasticsearch['plugins']['marvel-agent']['version']}"
+install_plugin "marvel-agent"
