2bfb4bde8f5e5e29d67493ed4053269d0c1766b1
polloelastico <merv@bigdumbtruck.com>
2016-02-04 10:39:53 -0800

    syntax for es plugin installer has changed.

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -3,7 +3,7 @@ script "install_plugin_es_head" do
 	user "root"
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
-  	plugin -install mobz/elasticsearch-head
+  	plugin install mobz/elasticsearch-head
   	EOH
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
